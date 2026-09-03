480276a4c9db6ed84e0a7def47de3e4af5f72ee1
M-Ola <ola466@gmail.com>
2026-09-03 06:12:14 -0500

    Finish assignment one

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+// Days until next Christmas
+var today = DateTime.Today;
+var nextChristmas = new DateTime(today.Year, 12, 25);
+
+var daysUntilChristmas = (nextChristmas - today).Days;
+
+Console.WriteLine($"There are {daysUntilChristmas} days until next Christmas.");
+
+
+    

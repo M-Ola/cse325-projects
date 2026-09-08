7a15a46a0be73e8d80a234b9a47dad4b74520087
M-Ola <ola466@gmail.com>
2026-09-08 08:33:45 -0500

    Change 'Index' to 'My Movies'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+998b7a2fad18ac4c120da9590442b03fdb739718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7c6a837b580c49c0029a4e5ffc4b8625df0dd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

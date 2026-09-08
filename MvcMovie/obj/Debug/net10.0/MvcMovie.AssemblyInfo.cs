1ae768b6089b4b86d06b173f23a5049f5156cfd7
M-Ola <ola466@gmail.com>
2026-09-08 09:17:57 -0500

    Add styling

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a15a46a0be73e8d80a234b9a47dad4b74520087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3177bfdadb6ed5b2a3b958a276f1fceb6b216688")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
